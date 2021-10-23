794274d12ebe637b75a913be6fd581e0853c0b6d
Tushar Garg <tushargarg0110@gmail.com>
2021-10-23 11:05:14 +0530

    Basics of JavaScript

--- Lec-3-JavaScript/practice.js
+++ Lec-3-JavaScript/practice.js
@@ -1,56 +1,56 @@
 
-//console.log(a);
+console.log(a);
 
 
-// let a = 10; //block
+let a = 10; //block
 
-// var b = 20; //function scope
+var b = 20; //function scope
 
-// c = 30; //Globally
+c = 30; //Globally
 
-// const pi = 3.14; //Global --they r constant and global
+const pi = 3.14; //Global --they r constant and global
 
-// e = 10;
-// function fun(){
+e = 10;
+function fun(){
 
-//     let a = 5;
-//     if(a==5){
-//        console.log("inside",e);
-//     }
-//     console.log("outside",e);
-// }
+    let a = 5;
+    if(a==5){
+       console.log("inside",e);
+    }
+    console.log("outside",e);
+}
 
-// console.log("Global scope is ", e);
-// fun();
+console.log("Global scope is ", e);
+fun();
 
-// first(10);
-// second();
-
-
-// function first(n){
-
-//     console.log("inside the first function");
-
-//     console.log(Math.sqrt(n));
-//     //return Math.sqrt(n);
-// }
+first(10);
+second();
 
 
-// second = function(){
+function first(n){
 
-//     console.log("inside the second function");
-//     return 10;
-// }
+    console.log("inside the first function");
+
+    console.log(Math.sqrt(n));
+    //return Math.sqrt(n);
+}
 
 
+second = function(){
 
+    console.log("inside the second function");
+    return 10;
+}
 
 
 
-// function second(n){
 
-//     console.log("inside the second function");
-// }
+
+
+function second(n){
+
+    console.log("inside the second function");
+}
 
 
 
